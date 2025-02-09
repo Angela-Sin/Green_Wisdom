b439321bc2e527d41abd44e5c2a00d41864ab75c
Angela-Sin <anzela.sinia@gmail.com>
2025-02-09 09:17:48 +0000

    True to False

--- main/settings.py
+++ main/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback_secret_key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
