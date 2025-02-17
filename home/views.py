e8cf9b7111ee1c9f15fcf5b2bbedf6a9e0ea52c7
Angela-Sin <anzela.sinia@gmail.com>
2025-02-17 14:36:17 +0000

    create index.html for home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,17 @@
-from django.views.generic import TemplateView
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.views.generic import ListView
+from blog.models import BlogPost
 
 
-
-class Index(TemplateView):
+class Index(ListView):
     template_name = 'home/index.html'
+    model = BlogPost
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+         return self.model.objects.all().order_by('-published_date')
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['welcome_message'] = "Welcome to the Gardening Blog!"
+        return context
